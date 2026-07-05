2c68343d5d166943670c97f5eec76200c02e7a38
Jonathan Elm <jonathan.elm@stud.hs-merseburg.de>
2026-07-05 16:30:51 +0200

    first idea

--- src/main/java/CollatzCalculator.java
+++ src/main/java/CollatzCalculator.java
@@ -1,7 +1,14 @@
 class CollatzCalculator {
 
     int computeStepCount(int start) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (start == 1){
+            return 1;
+        }    
+        if (start % 2 == 0){
+            return 1 + computeStepCount(start/2);
+        }else{
+            return 1 + computeStepCount(3* start + 1);
+        }
     }
 
 }
